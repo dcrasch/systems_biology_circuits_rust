1dc0e9cc565c320f8b2e9572f044e28fbd0bc8e4
David Rasch <david.rasch@cwi.nl>
2024-01-23 13:01:46 +0100

    working rust notebook

--- ch0/src/main.rs
+++ ch0/src/main.rs
@@ -15,19 +15,10 @@ fn autorepressive(
     ns: f64,
     s: f64,
 ) -> f64 {
-    beta0 * (s / ks).powf(ns) / (1.0 + (s / ks).powf(ns)) / (1.0 + (x / k).powf(n)) - gamma * x	
+    beta0 * (s / ks).powf(ns) / (1.0 + (s / ks).powf(ns)) / (1.0 + (x / k).powf(n)) - gamma * x
 }
 
-fn unregulated(
-    t: f64,
-    beta0: f64,
-    gamma: f64,
-    k: f64,
-    n: f64,
-    ks: f64,
-    ns: f64,
-    s: f64,
-) -> f64 {
+fn unregulated(t: f64, beta0: f64, gamma: f64, k: f64, n: f64, ks: f64, ns: f64, s: f64) -> f64 {
     beta0 / gamma * (1.0 - (-gamma * t).exp())
 }
 
@@ -44,9 +35,7 @@ struct Cs {
 impl ode_solvers::System<f64, State> for Cs {
     fn system(&self, t: Time, y: &State, dy: &mut State) {
         dy[0] = autorepressive(
-            y[0], t, self.beta0, self.gamma,
-	    self.k, self.n,
-	    self.ks, self.ns, self.s,
+            y[0], t, self.beta0, self.gamma, self.k, self.n, self.ks, self.ns, self.s,
         );
     }
 }
@@ -68,46 +57,47 @@ fn main() {
     let mut stepper = Rk4::new(system, 0.0, y0, 10.0, step_size);
     let res = stepper.integrate();
     let (x_out, y_out) = stepper.results().get();
-    
+
     let root = BitMapBackend::new("myplot.png", (640, 480)).into_drawing_area();
     root.fill(&WHITE).unwrap();
-    
+
     let mut chart = ChartBuilder::on(&root)
         .caption("Constant-input dynamics", ("sans-serif", 50).into_font())
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(0.0f64..10.0f64, 0.0f64..100.0f64).unwrap();
+        .build_cartesian_2d(0.0f64..10.0f64, 0.0f64..100.0f64)
+        .unwrap();
 
     chart.configure_mesh().draw().unwrap();
 
-    let points = x_out.iter().zip(y_out.iter())
-	.map(|(x,y)|(*x,y[0]))
-	.collect::<Vec<(f64,f64)>>();
+    let points = x_out
+        .iter()
+        .zip(y_out.iter())
+        .map(|(x, y)| (*x, y[0]))
+        .collect::<Vec<(f64, f64)>>();
     chart
-        .draw_series(LineSeries::new(
-            points,
-            &BLUE,
-        )).unwrap()
-	.label("autorepressive")
-	.legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+        .draw_series(LineSeries::new(points, &BLUE))
+        .unwrap()
+        .label("autorepressive")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
 
-    
-    let points = (0..200).map(|t|t as f64 * 0.05).map(|t|(t,unregulated(t,100.0,1.0,1.0,1.0,10.0,0.1,100.0)))
-	.collect::<Vec<(f64,f64)>>();
+    let points = (0..200)
+        .map(|t| t as f64 * 0.05)
+        .map(|t| (t, unregulated(t, 100.0, 1.0, 1.0, 1.0, 10.0, 0.1, 100.0)))
+        .collect::<Vec<(f64, f64)>>();
     chart
-        .draw_series(LineSeries::new(
-            points,
-            &RED,
-        )).unwrap()
-	.label("unregulated")
-	.legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-    
+        .draw_series(LineSeries::new(points, &RED))
+        .unwrap()
+        .label("unregulated")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
     chart
         .configure_series_labels()
         .background_style(&WHITE.mix(0.8))
         .border_style(&BLACK)
-        .draw().unwrap();
+        .draw()
+        .unwrap();
 
     root.present().unwrap();
 }
